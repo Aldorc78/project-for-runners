6ab74c740cef30c5dd7c1efd166e3759b5387cbc
Aldo Rosete <aldorc78@gmail.com>
2024-04-11 19:05:33 -0600

    adding some math to calculate aerobic heart rate zones

--- project-for-runners.py
+++ project-for-runners.py
@@ -1,7 +1,9 @@
 class Runner:
     maxHeartRate = 0
     age = 0
-    weightKg = 0    
+    weightKg = 0 
+
+    aerobicZones = {"zone1": [], "zone2": [],"zone3": [], "zone4": [], "zone5": []}
 
     def __init__(self, name, gender, weightKg, age, minHeartRate):
         
@@ -25,7 +27,6 @@ class Runner:
         else:
             self.age = age
 
-
         #This section sets your max heartrate using specific formulas based on your gender
         if self.gender == "male":
             self.maxHeartRate = 220 - self.age # We use Fox method for males
@@ -35,5 +36,45 @@ class Runner:
             print("Your Max HeartRate is: " + str(self.maxHeartRate))
         else:
             print("Please select a correct gender, max heartrate stays on 0")
-    
-aldo = Runner("Aldo", "Male", 78, -2, 46)
+        
+        #This for section is going to calculate the aerobic zones of the runner
+        #each zone is a range so they have two limits 
+        lLimitZone1 = (50*self.maxHeartRate)/100 
+        uLimitZone1 = (60*self.maxHeartRate)/100 
+
+        lLimitZone2 = (61*self.maxHeartRate)/100 
+        uLimitZone2 = (70*self.maxHeartRate)/100 
+
+        lLimitZone3 = (71*self.maxHeartRate)/100 
+        uLimitZone3 = (80*self.maxHeartRate)/100    
+
+        lLimitZone4 = (81*self.maxHeartRate)/100 
+        uLimitZone4 = (90*self.maxHeartRate)/100      
+
+        lLimitZone5 = (91*self.maxHeartRate)/100 
+        uLimitZone5 = (100*self.maxHeartRate)/100    
+
+        #i = 1
+        #for zone in self.aerobicZones:
+          #  self.aerobicZones["zone{}".format(i)].append(lLimitZone1)
+           # self.aerobicZones["zone{}".format(i)].append(uLimitZone1) 
+            
+
+
+        self.aerobicZones["zone1"].append(lLimitZone1) 
+        self.aerobicZones["zone1"].append(uLimitZone1) 
+        self.aerobicZones["zone2"].append(lLimitZone2) 
+        self.aerobicZones["zone2"].append(uLimitZone2)         
+        self.aerobicZones["zone3"].append(lLimitZone3) 
+        self.aerobicZones["zone3"].append(uLimitZone3)            
+        self.aerobicZones["zone4"].append(lLimitZone4) 
+        self.aerobicZones["zone4"].append(uLimitZone4)      
+        self.aerobicZones["zone5"].append(lLimitZone5) 
+        self.aerobicZones["zone5"].append(uLimitZone5)            
+      
+
+
+
+aldo = Runner("Aldo", "Male", 78, 23, 46)
+
+print(aldo.aerobicZones)
